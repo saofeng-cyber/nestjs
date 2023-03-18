cd1076d1c8ade18f4d07f12cecbb97355b0f9f4e
sunminggang <942391953@qq.com>
2023-03-19 03:30:07 +0800

    修改过期时间

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '2days' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
